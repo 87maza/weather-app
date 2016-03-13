5defa410dea3d7b2fd30bced39d11afc415141bb
87maza <87maza@gmail.com>
2016-03-13 16:40:41 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+
+//create an object with random pictures that relate to time and weather of forecast
+//http://nominature.com/wp-content/uploads/2015/12/sunsets-calm-afternoon-lake-beautiful-sunset-clouds-building-trees-walkway-wallpaper-pictures-free.jpg
+//http://www.hdwallpaperup.com/wp-content/uploads/2015/01/Sunset-on-Malibu-Beach-California.jpg
+//http://archwall.xyz/wp-content/uploads/2015/12/other-chinese-market-photography-china-nightlife-wonderful-great-skyphoenixx1-amazing-evening-lights-night-awesome-outstanding-adorable-stunning-nice-fantastic-marvellous-trees-architecture-.jpg
+//http://2.bp.blogspot.com/_UeKpPmoedmk/TJttTsVSoZI/AAAAAAAAAik/161bEYBfUuc/s1600/Black%2Band%2BWhite%2BLos%2BAngeles%2BHarbor%2BWalp%2BTLG.png
+//
+
